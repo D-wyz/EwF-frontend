36c7f582c18127828ace2f8b86c626b271cf2b3d
AgustinGarcia <aagarcia@protonmail.com>
2018-10-15 15:42:27 -0400

    accept changes

--- src/views/Components/Header/Header.jsx
+++ src/views/Components/Header/Header.jsx
@@ -8,10 +8,6 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-<<<<<<< HEAD
-
-=======
->>>>>>> master
 import HeaderLinks from "components/Header/HeaderLinks.jsx";
 import CustomDropdown from "components/CustomDropdown/CustomDropdown.jsx";
 // import Button from "components/CustomButtons/Button.jsx";
@@ -32,10 +28,6 @@ const styles = {
 
 function ButtonAppBar(props) {
   const { classes } = props;
-<<<<<<< HEAD
-
-=======
->>>>>>> master
   return (
     <div className={classes.root}>
       <AppBar position="static">
